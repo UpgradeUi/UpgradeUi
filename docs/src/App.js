72cb19697f46fdc9b1d746c0aa206288f0dbc339
Specter <74319234+e8x@users.noreply.github.com>
2022-02-27 10:32:35 +0200

    0

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes,  } from "react-router-dom";
-import Index from "./components/pages/index";
+import Index from "./components/pages/index/index";
 
 function App() {
   return (
---
+++ docs/src/components/pages/index/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../upgradeui/upgrade.css";
+
+function index() {
+  return (
+    <>
+      <div className="h-screen flex bg-f100">
+        <div className="w-screen left block p-14 h-48 bg-transparent">
+          <a className="text-white p-14 block-flex">Home</a>
+          <a className="text-white p-14 block-flex">Docs</a>
+          <a className="text-white p-14 block-flex">Components</a>
+          <h1 className="text-white ai-center">
+            Create modern websites quickly without hassle and hassles
+          </h1>
+        </div>
+
+      </div>
+    </>
+  );
+}
+export default index;
